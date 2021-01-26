ea3bae3932edfad5fbf028c6d0759f2c321107a4
Juliano Costa Silva <apfjuliano@gmail.com>
2021-01-25 22:38:24 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './App.css';
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Home from './Components/Home'
+import Login from './Components/Login'
+
 function App() {
-  return <div>Facedogs</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login}  />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,15 @@
 import styles from './Header.module.css'
 import { Link } from 'react-router-dom'
+import {ReactComponent as Dogs} from '../Assets/dogs.svg'
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav>
-        <Link to='/'>Home</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to='/' className={styles.logo} aria-label='Go to Dogs Home'><Dogs /></Link>
+        <Link to='/login' className={styles.login} aria-label='Go to Login area'>Login / Sign-in</Link>
       </nav>
-    </div>
+    </header>
   )
 }
 
