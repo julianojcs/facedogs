26a616954db1ff9375261eea21f302390901e32a
Juliano Costa Silva <apfjuliano@gmail.com>
2021-02-02 15:17:39 -0300

    Infinite Scroll

--- src/Components/User/index.js
+++ src/Components/User/index.js
@@ -1,17 +1,22 @@
+import { useContext } from 'react'
 import UserHeader from './UserHeader'
 import { useRouteMatch, Route, Switch } from 'react-router-dom'
 import Feed from '../Feed'
 import UserPhotoPost from './UserPhotoPost'
 import UserStats from './UserStats'
+import { UserContext } from '../../UserContext'
 
 const User = () => {
   const { path } = useRouteMatch()
+  const { data } = useContext(UserContext)
 
   return (
     <section className='container'>
       <UserHeader />
       <Switch>
-        <Route exact path={`${path}/`} component={Feed} />
+        <Route exact path={`${path}/`}>
+          <Feed user={data.id} />
+        </Route>
         <Route path={`${path}/post`} component={UserPhotoPost} />
         <Route path={`${path}/statistics`} component={UserStats} />
       </Switch>
