95af95437eca4cafca778319e8a9fd7d70211e87
Juliano Costa Silva <apfjuliano@gmail.com>
2021-02-02 00:17:43 -0300

    Comments Posts

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,5 +1,25 @@
-const PhotoComments = () => {
-  return <div></div>
+import { useState, useContext } from 'react'
+import { UserContext } from '../../UserContext'
+import PhotoCommentsForm from './PhotoCommentsForm'
+import styles from './PhotoComments.module.css'
+
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments)
+  const { login } = useContext(UserContext)
+
+  return (
+    <>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  )
 }
 
 export default PhotoComments
