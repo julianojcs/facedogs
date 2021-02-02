ef775d10dc7d6aa742a025022a6b655e0333e286
Juliano Costa Silva <apfjuliano@gmail.com>
2021-02-02 15:54:26 -0300

    addEventListener touchmove for mobile

--- src/Components/Feed/index.js
+++ src/Components/Feed/index.js
@@ -7,27 +7,30 @@ const Feed = ({ user }) => {
   const [pages, setPages] = useState([1])
   const [infinite, setInfinite] = useState(true);
 
-  useEffect(() => {
-    let wait = false;
+  useEffect(( ) => {
+    let wait = false
     const infiniteScroll = () => {
       if (infinite) {
-        const scroll = window.scrollY;
-        const height = document.body.offsetHeight - window.innerHeight;
+        const scroll = window.scrollY
+        const height = document.body.offsetHeight - window.innerHeight
         if (scroll > height * 0.85 && !wait) {
-          setPages((pages) => [...pages, pages.length + 1]);
-          wait = true;
+          setPages((pages) => [...pages, pages.length + 1])
+          wait = true
           setTimeout(() => {
-            wait = false;
-          }, 500);
+            wait = false
+          }, 500)
         }
       }
     }
     window.addEventListener('wheel', infiniteScroll)
     window.addEventListener('scroll', infiniteScroll)
+    window.addEventListener('touchmove', infiniteScroll)  //! for mobile
+    
     //! Clear/Remove those events
     return () => {
       window.removeEventListener('wheel', infiniteScroll)
       window.removeEventListener('scroll', infiniteScroll)
+      window.removeEventListener('touchmove', infiniteScroll)
     }
   }, [infinite])
 
