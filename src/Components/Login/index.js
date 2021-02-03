a2354088e617ea4c8a05221097db22f3a9591fae
Juliano Costa Silva <apfjuliano@gmail.com>
2021-02-03 16:56:21 -0300

    Password Reset

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -3,7 +3,7 @@ import LoginForm from './LoginForm'
 import LoginCreate from './LoginCreate'
 import LoginPwdRecover from './LoginPwdRecover'
 import LoginPswReset from './LoginPswReset'
-import { Route, useRouteMatch, Redirect } from 'react-router-dom'
+import { Switch, Route, useRouteMatch, Redirect } from 'react-router-dom'
 import { UserContext } from '../../UserContext'
 import styles from './Login.module.css'
 import PageNotFound from '../404'
@@ -16,11 +16,13 @@ function Login() {
   return (
     <section className={styles.login}>
       <div className={styles.forms}>
-        <Route exact path={`${path}/`} component={LoginForm} />
-        <Route path={`${path}/create`} component={LoginCreate} />
-        <Route path={`${path}/recover`} component={LoginPwdRecover} />
-        <Route path={`${path}/reset`} component={LoginPswReset} />
-        <Route path={`${path}/*`} component={PageNotFound} />
+        <Switch>
+          <Route exact path={`${path}/`} component={LoginForm} />
+          <Route path={`${path}/create`} component={LoginCreate} />
+          <Route path={`${path}/recover`} component={LoginPwdRecover} />
+          <Route path={`${path}/reset`} component={LoginPswReset} />
+          <Route path={`${path}/*`} component={PageNotFound} />
+        </Switch>
       </div>
     </section>
   )
