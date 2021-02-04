cf75392404216b6a42b02933bb6e1c337a99a6b3
Juliano Costa Silva <apfjuliano@gmail.com>
2021-02-04 02:43:00 -0300

    Final refactoring

--- src/Components/Login/LoginPwdRecover.js
+++ src/Components/Login/LoginPwdRecover.js
@@ -24,7 +24,7 @@ const LoginPasswordRecover = () => {
   }
 
   return (
-    <section>
+    <section className='animeLeft'>
       <Head title='Recupere sua senha' />
       <h1 className='title'>Perdeu a senha?</h1>
       {data ? (
